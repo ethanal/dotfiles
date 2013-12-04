dc1edffef8b2a70bffe21ba076d0cf3542686126
Ethan Lowman <ethan.l.527@gmail.com>
2013-12-04 02:57:26 -0500

    Actually fix welcome message

--- term/welcome.zsh
+++ term/welcome.zsh
@@ -1,4 +1,4 @@
-if test $(which cowsay)
+if command -v cowsay > /dev/null
 then
     echo -e "\e[31m$(python -c "from datetime import date; print str((date(date.today().year, 12, 21) - date.today()).days) + ' until Winter Break'" | cowsay -f moose)"
 fi
