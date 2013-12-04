fb421a177c43d5d4288fb43b4397f01b32544551
Ethan Lowman <ethan.l.527@gmail.com>
2013-12-04 02:33:14 -0500

    Only run welcome script if cowsay exists

--- term/welcome.zsh
+++ term/welcome.zsh
@@ -1,1 +1,4 @@
-echo -e "\e[31m$(python -c "from datetime import date; print str((date(date.today().year, 12, 21) - date.today()).days) + ' until Winter Break'" | cowsay -f moose)"
+if test ! $(which cowsay)
+then
+    echo -e "\e[31m$(python -c "from datetime import date; print str((date(date.today().year, 12, 21) - date.today()).days) + ' until Winter Break'" | cowsay -f moose)"
+fi
