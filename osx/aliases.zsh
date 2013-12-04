6be88ab065bb79584d997c7810600b7c322f2eaa
Ethan Lowman <ethan.l.527@gmail.com>
2013-12-03 23:43:44 -0500

    Fix show/hide hidden files aliases

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,6 +1,6 @@
 alias fixau=sudo killall -9 appleeventsd
-alias showhidden=defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder
-alias hidehidden=defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder
+alias showhidden="defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder"
+alias hidehidden="defaults write com.apple.finder AppleShowAllFiles FALSE && killall Finder"
 alias man2pdf="man -t '${1}' | open -f -a /Applications/Preview.app"
 alias quicklook=qlmanage -p "${1}"
 
