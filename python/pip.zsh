64f013002a4c56191c9930c059d0f640483b9a06
Ethan Lowman <ethan.l.527@gmail.com>
2016-06-16 22:37:30 -0400

    Comment out virtualenv stuff for now

--- python/pip.zsh
+++ python/pip.zsh
@@ -1,26 +1,21 @@
-if [ $(uname -s) = "Darwin" ] 
-then
-    # virtualenv should use Distribute instead of legacy setuptools
-    export VIRTUALENV_DISTRIBUTE=false
-    # Centralized location for new virtual environments
-    export PIP_VIRTUALENV_BASE=$HOME/.virtualenvs
-    # pip should only run if there is a virtualenv currently activated
-    # export PIP_REQUIRE_VIRTUALENV=true
-    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+# if [ $(uname -s) = "Darwin" ] 
+# then
+#     # virtualenv should use Distribute instead of legacy setuptools
+#     export VIRTUALENV_DISTRIBUTE=false
+#     # Centralized location for new virtual environments
+#     export PIP_VIRTUALENV_BASE=$HOME/.virtualenvs
+#     # pip should only run if there is a virtualenv currently activated
+#     # export PIP_REQUIRE_VIRTUALENV=true
+#     export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+# 
+#     source /usr/local/bin/virtualenvwrapper.sh 
+# elif [[ $(hostname) =~ .*ethan.* ]]
+# then
+#     export VIRTUALENV_DISTRIBUTE=false
+#     export PIP_VIRTUALENV_BASE=/usr/local/virtualenvs
+#     export PIP_DOWNLOAD_CACHE=/usr/local/virtualenvs/cache
+#     export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+#     export WORKON_HOME=/usr/local/virtualenvs
+#     source /usr/local/bin/virtualenvwrapper.sh
+# fi
 
-    source /usr/local/bin/virtualenvwrapper.sh 
-elif [[ $(hostname) =~ .*ethan.* ]]
-then
-    export VIRTUALENV_DISTRIBUTE=false
-    export PIP_VIRTUALENV_BASE=/usr/local/virtualenvs
-    export PIP_DOWNLOAD_CACHE=/usr/local/virtualenvs/cache
-    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
-    export WORKON_HOME=/usr/local/virtualenvs
-    source /usr/local/bin/virtualenvwrapper.sh
-fi
-
-
-if [ "$(hostname)" = "ion" ]
-then
-    source /usr/local/bin/ion_development_env_setup.sh
-fi
