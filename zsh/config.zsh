ee1bbca6b7c9ccd1c7ac8853626c36b7aec3373f
Ethan Lowman <ethan.l.527@gmail.com>
2018-02-16 12:01:29 -0500

    Fix zsh shared history

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,10 +2,9 @@ export CLICOLOR=true
 export DISABLE_AUTO_TITLE=true
 export HISTFILESIZE=200000
 export HISTSIZE=10000
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
-unsetopt sharehistory
 setopt interactivecomments
 export COWPATH=$HOME/.dotfiles/term/cows:/usr/local/share/cows
