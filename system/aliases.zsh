9301c3ae2db79c31a70bb54ed79853b65750d30d
Ethan Lowman <ethan.l.527@gmail.com>
2018-02-16 12:00:43 -0500

    No need to alias keybase any more

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,5 @@
 alias pg="gist -pc"
-alias keybase="GPG_TTY=$(tty) keybase"
+# alias keybase="GPG_TTY=$(tty) keybase"
 alias gpg="gpg2"
 alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | echo 'Public key copied to pasteboard.'"
 
