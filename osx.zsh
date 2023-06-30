b6885644acbe60b7565d31ef544060b55b8c6b4a
Ethan Lowman <ethan.l.527@gmail.com>
2023-06-30 00:06:37 -0400

    updates

--- osx.zsh
+++ osx.zsh
@@ -2,3 +2,7 @@ defaults write -g NSRequiresAquaSystemAppearance -bool Yes
 defaults write com.apple.notificationcenterui NSRequiresAquaSystemAppearance -bool No
 defaults write com.apple.Spotlight NSRequiresAquaSystemAppearance -bool No
 defaults write com.manytricks.Moom NSRequiresAquaSystemAppearance -bool No
+
+# Prevent Yubikey from draining battery
+sudo pmset -b hibernatemode 25
+sudo pmset -b standbydelay 300
