e943b45bb091e7e92f3d515676fb2f5bb98fbc39
Ethan Lowman <git@ethanlowman.com>
2024-12-03 23:11:14 -0500

    Fix python indent

--- nvim.symlink/plugin/nvim-treesitter.lua
+++ nvim.symlink/plugin/nvim-treesitter.lua
@@ -36,7 +36,9 @@ require'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    --
+    -- https://github.com/nvim-treesitter/nvim-treesitter/issues/1573
+    additional_vim_regex_highlighting = { "python" },
   },
 }
 
