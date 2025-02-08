7490bc92a8ff0ce6014a22e8dceb3d8b4e57d708
Ethan Lowman <git@ethanlowman.com>
2025-02-07 19:19:01 -0500

    updates

--- funcs.zsh
+++ funcs.zsh
@@ -71,7 +71,7 @@ jj-sync() {(
   set -euo pipefail
   DEFAULT_BRANCH=$(jj log -r "local_trunk()" -T "local_bookmarks.filter(|b| b.name() == 'master' || b.name() == 'main' || b.name() == 'trunk')" --no-pager --no-graph --color=never)
   set -x
-  jj git fetch -b $DEFAULT_BRANCH -b "glob:ethan/*"
+  jj git fetch -b $DEFAULT_BRANCH -b "glob:$USER/*"
   jj bookmark set $DEFAULT_BRANCH --to "trunk()"
 )}
 
@@ -81,6 +81,75 @@ jj-restack() {(
 )}
 
 jj-submit-all() {(
-  set -euxo pipefail
-  jj git push -r 'mine() & trunk().. & bookmarks("glob:ethan/*")'
+  set -euo pipefail
+
+  for CHANGE_ID in $(jj log -r "mine() & trunk().. & bookmarks(glob:'$USER/*')" --no-pager --no-graph --color=never -T 'change_id ++ "\n"'); do
+    echo Submitting $CHANGE_ID...
+    jj-submit $CHANGE_ID
+    echo
+  done
 )}
+
+jj-submit() {(
+  set -euo pipefail
+
+  COMMIT=$1;
+  CHANGE_ID="$(jj log --template 'change_id.shortest(8)' --no-pager --no-graph --color=never -r $COMMIT)"
+  BRANCH_NAME="$USER/$(jj log --template 'change_id.shortest(8)' --no-pager --no-graph --color=never -r $CHANGE_ID)"
+
+  MQ_CHANGE_ID=$(jj log -T 'if(parents.len() == 1, parents.map(|p| p.change_id()))' -r $COMMIT --no-pager --no-graph --color=never)
+  MQ_BRANCH_NAME="mq/$CHANGE_ID"
+
+  set -x
+  jj bookmark set $BRANCH_NAME -r $CHANGE_ID
+  jj bookmark set $MQ_BRANCH_NAME -r $MQ_CHANGE_ID
+
+  jj git push -b exact:$MQ_BRANCH_NAME --allow-new
+  jj git push -b exact:$BRANCH_NAME --allow-new
+  set +x
+  if [[ "$(gh pr list --head $BRANCH_NAME --json number)" == "[]" ]]; then
+    echo Creating PR...
+    gh pr create --head $BRANCH_NAME --base $MQ_BRANCH_NAME
+  else
+    # Reset the base. Seems to be necessary to get the PR to update.
+    echo Updating PR...
+    gh pr edit $BRANCH_NAME --base $MQ_BRANCH_NAME
+  fi
+
+  PR_NUMBER=$(gh pr list --head $BRANCH_NAME --json number | jq ".[] | .number")
+
+  # Build PR comment.
+  LOG=$(jj log -r "(trunk()::$CHANGE_ID | $CHANGE_ID::) & remote_bookmarks(glob:'$USER/*')" --no-pager --color=never -T "description.first_line() ++ ' REPLACE_BRANCH_NAME:' ++ remote_bookmarks.filter(|b| b.name().starts_with('$USER/') && b.remote() == 'git').map(|b| b.name())")
+  echo $LOG | grep -o 'REPLACE_BRANCH_NAME.\+' | while read -r PATTERN; do
+    STK_BRANCH_NAME=$(echo $PATTERN | sed -E 's/REPLACE_BRANCH_NAME:(.*)/\1/')
+    PR_URL="<a href='$(gh pr view $STK_BRANCH_NAME --json url --template "{{ .url }}")'>#$PR_NUMBER</a>"
+    PATTERN_ESCAPE=$(echo $PATTERN | sed -E 's/\//\\\//g')
+    PR_URL_ESCAPE=$(echo $PR_URL | sed -E 's/\//\\\//g')
+
+    SUFFIX=""
+    if [[ $STK_BRANCH_NAME == $BRANCH_NAME ]]; then
+      SUFFIX=" 👈"
+    fi
+    LOG=$(echo $LOG | sed -E "s/$PATTERN_ESCAPE/$PR_URL_ESCAPE$SUFFIX/g")
+  done
+
+  COMMENT_FILE=$(mktemp)
+  echo "### 🥞 Pull Request Stack" >> $COMMENT_FILE
+  echo '<pre>' >> $COMMENT_FILE
+  echo $LOG >> $COMMENT_FILE
+  echo '</pre>' >> $COMMENT_FILE
+  # echo $LOG | sed -E 's/.+/<samp>&<\/samp>/g' >> $COMMENT_FILE
+  echo "<!-- this_is_the_jj_pr_stack_comment -->" >> $COMMENT_FILE
+
+  # Create or update PR comment.
+  OWNER_REPO=$(gh repo view --json owner,name --template "{{ .owner.login }}/{{ .name }}")
+  EXISTING_COMMENT=$(gh api --method GET repos/${OWNER_REPO}/issues/${PR_NUMBER}/comments | jq '.[] | select(.body | contains("this_is_the_jj_pr_stack_comment")) | .id')
+  if [[ -n $EXISTING_COMMENT ]]; then
+    gh api --method PATCH repos/${OWNER_REPO}/issues/comments/${EXISTING_COMMENT} -F body=@$COMMENT_FILE --silent
+  else
+    gh pr comment $PR_NUMBER --body-file $COMMENT_FILE > /dev/null
+  fi
+
+  rm $COMMENT_FILE
+)}
+
