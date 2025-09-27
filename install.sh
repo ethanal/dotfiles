50b5ce94b50b3f087a197a711d11c89a8a22930f
Ethan Lowman <git@ethanlowman.com>
2025-09-27 19:29:20 -0400

    disable ohmyzsh stuff

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 mkdir -p $HOME/.tmp
 touch local.zsh
 # TODO: fix for nix
-mkdir -p ~/.oh-my-zsh/custom/themes
-ln -sf $PWD/ethan.zsh-theme $HOME/.oh-my-zsh/custom/themes/ethan.zsh-theme
+# mkdir -p ~/.oh-my-zsh/custom/themes
+# ln -sf $PWD/ethan.zsh-theme $HOME/.oh-my-zsh/custom/themes/ethan.zsh-theme
 
 ln -sf $PWD/zshrc.symlink $HOME/.zshrc
 ln -sfT $PWD/nvim.symlink $HOME/.config/nvim
