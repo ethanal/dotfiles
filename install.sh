9b3792f4fca5419238062043d1dcd572420b4784
Ethan Lowman <git@ethanlowman.com>
2026-08-14 21:53:34 -0400

    fix install in ro fs

--- install.sh
+++ install.sh
@@ -19,10 +19,19 @@ ln -sf $PWD/ripgreprc $HOME/.ripgreprc
 ln -sfT $PWD/nvim $HOME/.config/nvim
 ln -sfT $PWD/ghostty $HOME/.config/ghostty
 
-wget -O zellij/plugins/vim-zellij-navigator.wasm https://github.com/hiasr/vim-zellij-navigator/releases/download/0.3.0/vim-zellij-navigator.wasm
-echo "77e5a2f62f7c1a698caf2574493d5d75587e0fd877cfba34c48caf731c24c58d  zellij/plugins/vim-zellij-navigator.wasm" | sha256sum --check
+fetch_with_hash() {
+  url="$1"
+  dest="$2"
+  hash="$3"
+  if [ -f "$dest" ] && echo "$hash  $dest" | sha256sum --check --status; then
+    return
+  fi
+  wget -O "$dest" "$url"
+  echo "$hash  $dest" | sha256sum --check
+}
 
-wget -O zellij/plugins/zjstatus.wasm https://github.com/dj95/zjstatus/releases/download/v0.23.0/zjstatus.wasm
-echo "e006901223524239db618021e4cc5d17f82dc4bfae5432895ba41f03f13861ff  zellij/plugins/zjstatus.wasm" | sha256sum --check
+fetch_with_hash https://github.com/hiasr/vim-zellij-navigator/releases/download/0.3.0/vim-zellij-navigator.wasm zellij/plugins/vim-zellij-navigator.wasm 77e5a2f62f7c1a698caf2574493d5d75587e0fd877cfba34c48caf731c24c58d
+
+fetch_with_hash https://github.com/dj95/zjstatus/releases/download/v0.23.0/zjstatus.wasm zellij/plugins/zjstatus.wasm e006901223524239db618021e4cc5d17f82dc4bfae5432895ba41f03f13861ff
 
 ln -sfT $PWD/zellij $HOME/.config/zellij
